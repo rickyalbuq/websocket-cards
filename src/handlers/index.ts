bcfdbf3b3d3efd87c2e5a7e9668006c2fde0e8be
Henrique Albuquerque <ehf.albuquerque@gmail.com>
2022-07-31 21:21:26 -0300

    🎨 Improving Rooms Handlers

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,12 +1,13 @@
 import { sendMessage } from "./messages.handlers";
 import { connectPlayers, disconnectPlayer } from "./players.handlers";
-import { createRoom, deleteRoom, getPublicRooms } from "./rooms.handlers";
+import { createRoom, deleteRoom, getPublicRooms, getRoomById } from "./rooms.handlers";
 
 export {
   connectPlayers,
   createRoom,
   deleteRoom,
   disconnectPlayer,
+  getRoomById,
   getPublicRooms,
   sendMessage
 };
--- src/handlers/rooms.handlers.ts
+++ src/handlers/rooms.handlers.ts
@@ -1,66 +1,44 @@
-import { Room } from "../models";
-import { Socket } from "socket.io";
 import * as I from "../interfaces";
+import { Socket } from "socket.io";
+import { Player, Room } from "../models";
 
-let rooms: I.Room[] = [];
-let players: I.Player[] = [];
+async function createRoom(data: I.CreateRoomData, socket: Socket) {
+  const room = await Room.findOne({where: {"roomId": data.roomId}, raw: true});
 
-function createRoom(data: I.CreateRoomData, socket: Socket) {
-  //(room) => item.room === data.room
-
-  Room.findOne()
-    .then((room) => {
-      if(room) {
-        socket.emit("createRoom", JSON.stringify(
-          {
-            "payload": {
-              "message": "Room already exists"
-            }
-          })
-        );
-      } else {
-        const createdAt = String(new Date().getTime());
-
-        const newRoom = {
-          room: data.room,
-          createdAt: createdAt,
-          isPrivate: data.isPrivate,
-          maxMatches: data.maxMatches,
-          currentMatch: 1
-        };
-
-        Room.create(newRoom)
-          .then(() => {
-            socket.emit("createRoom", JSON.stringify(
-              {
-                "payload": {
-                  "room": data.room,
-                  "createdAt": createdAt,
-                  "isPrivate": data.isPrivate,
-                  "maxMatches": data.maxMatches,
-                  "currentMatch": 1,
-                  "playerId": socket.id,
-                  "nickyname": data.username,
-                  "victories": 0,
-                  "message": "Room created successfully"
-                }
-              })
-            );
-          })
-          .catch((err) => console.log(err));
+  if(room) {
+    socket.emit("createRoom", JSON.stringify(
+      {
+        "payload": {
+          "message": "Room already exists"
         }
-      }
-    )
-    .finally(() => socket.join(`${data.room}`));
+      })
+    );
+  } else {
+    const newRoom = await Room.create({
+      roomId: data.roomId,
+      isPrivate: data.isPrivate,
+      maxMatches: data.maxMatches
+    });
+
+    socket.emit("createRoom", JSON.stringify(
+      {
+        "payload": {
+          "createdAt": newRoom?.createdAt,
+          "playerId": socket.id,
+          "message": "Room created successfully"
+        }
+      })
+    );
+
+    socket.join(`${data.roomId}`);
+  }
 }
 
-function deleteRoom(data: I.DeleteRoomData, socket: Socket) {
-  const roomHasPlayers = players.filter(
-    (player) => player.room === data.room
-  );
+async function deleteRoom(data: I.DeleteRoomData, socket: Socket) {
+  const roomHasPlayers = await Player.findAll({where: {roomId: data.roomId}});
 
   if(roomHasPlayers.length > 1) {
-    socket.emit("deleteRoom", () => JSON.stringify(
+    socket.emit("deleteRoom", JSON.stringify(
       {
         "payload": {
           "message": "Room is not empty"
@@ -68,43 +46,81 @@ function deleteRoom(data: I.DeleteRoomData, socket: Socket) {
       })
     );
   } else {
-    rooms = rooms.filter(
-      (room) => room.room !== data.room
-    );
-    players = players.filter(
-      (player) => player.room !== data.room
+    Room.destroy({where: {roomId: data.roomId}});
+
+    socket.emit("deleteRoom", JSON.stringify(
+      {
+        "payload": {
+          "message": "Room deleted successfully"
+        }
+      })
     );
   }
 }
 
-function getPublicRooms(socket: Socket) {
-  const bla: I.Room[] = [];
+async function getRoomById(data: I.GetRoomById, socket: Socket) {
+  const room = await Room.findOne({where: {roomId: data.roomId}});
 
-  if(rooms.length > 0) {
-    const publicRooms = bla.filter((room) => room.isPrivate === false);
-    const payload: I.GetRoomsData[] = [];
-
-    publicRooms.map((room) => {
-      const numberOfPlayers = players.filter(
-        (player) => player.room === room.room
-      );
-
-      payload.push({
-        ...room,
-        "messages": [],
-        "players": numberOfPlayers.length
-      });
-    });
-
-    socket.emit("getRooms", 
+  if(room) {
+    socket.emit("getRoomById", 
       JSON.stringify({
         "payload": {
-          "rooms": payload,
+          "roomId": room.roomId,
+          "isPrivate": room.isPrivate,
+          "maxMatches": room.maxMatches ||  10,
+          "currentMatch": room.currentMatch || 1,
+          "createdAt": room.createdAt,
           "playerId": socket.id,
-          "message": "Got rooms successfully"
+          "message": "Got room successfully"
         }
       })
     );
+
+    socket.join(`${data.roomId}`);
+  } else {
+    socket.emit("getRoomById", JSON.stringify(
+      {
+        "payload": {
+          "message": "Room not exists"
+        }
+      })
+    );
+  }
+}
+
+async function getPublicRooms(socket: Socket) {
+  let payload: I.GetRoomsData[] = [];
+  const rooms = await Room.findAll({where: {isPrivate: false}, raw: true});
+
+  if(rooms.length > 0) {
+    rooms.map((room) => {
+      Player.findAll({where: {roomId: room.roomId}}).then(
+        (players) => {
+          payload.push({
+            "roomId": room.roomId,
+            "isPrivate": room.isPrivate,
+            "maxMatches": room.maxMatches ||  10,
+            "currentMatch": room.currentMatch || 1,
+            "createdAt": room.createdAt,
+            "players": players.length
+          });
+        }
+      )
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        socket.emit("getRooms", 
+          JSON.stringify({
+            "payload": {
+              "rooms": payload,
+              "playerId": socket.id,
+              "message": "Got rooms successfully"
+            }
+          })
+        )
+      });
+    });
   } else {
     socket.emit("getRooms", 
       JSON.stringify({
@@ -116,4 +132,4 @@ function getPublicRooms(socket: Socket) {
   }
 }
 
-export { createRoom, deleteRoom, getPublicRooms };
+export { createRoom, deleteRoom, getRoomById, getPublicRooms };
